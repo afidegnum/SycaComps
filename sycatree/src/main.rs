bb1fd7747879a13893d7be7e2c7d6bcfa247c8c5
afidegnum <afidegnum@yahoo.com>
2023-03-11 11:05:47 +0000

    TreeView Rework, Stage1: Dnd stable

--- sycatree/src/main.rs
+++ sycatree/src/main.rs
@@ -1,5 +1,8 @@
+// https://htmldom.dev/make-a-draggable-element/
+
 use gloo::console::log;
 use serde::{Deserialize, Serialize};
+
 use sycamore::prelude::*;
 use wasm_bindgen::*;
 use web_sys::{DataTransfer, Event};
@@ -54,45 +57,21 @@ pub struct NodeState {
     pub nodes: RcSignal<NodeList>,
 }
 
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq, Hash)]
+struct ContentItem {
+    id: i32,
+    name: String,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+struct Contents {
+    contents: Vec<ContentItem>,
+}
+
 #[component(inline_props)]
-fn NestedNode<'a, G: Html>(cx: Scope<'a>, n: Node, items: &'a Signal<Vec<Node>>) -> View<G> {
-    // modif
-    let node_ref = create_node_ref::<G>(cx);
-    let node = n.clone();
-    let nd = n.clone();
-    let nodes = items.get();
-    let top_children = n
-        .get_immediate_children(&nodes)
-        .into_iter()
-        .cloned()
-        .collect();
-
-    let children_signal = create_signal(cx, top_children);
-
-    let toggle_state = create_signal(cx, false);
-
-    let toggle = |_| {
-        if *toggle_state.get() {
-            toggle_state.set(false)
-        } else {
-            toggle_state.set(true)
-        }
-    };
-
-    let class = move || {
-        format!(
-            "fa-regular {}",
-            if *toggle_state.get() {
-                "fa-square-minus"
-            } else if n.has_child(&nodes) {
-                "fa-square-plus"
-            } else {
-                ""
-            }
-        )
-    };
-
-    let c_item = create_signal(cx, nd);
+fn Item<'a, G: Html>(cx: Scope<'a>, n: Node, nodes_sig: &'a Signal<Vec<Node>>) -> View<G> {
+    let node_ref = create_node_ref(cx);
+    let node_signal = create_signal(cx, n);
     let handle_dragstart = |e: Event| {
         let dom = node_ref.get::<DomNode>();
         let drag_event_ref: &web_sys::DragEvent = e.unchecked_ref();
@@ -101,107 +80,99 @@ fn NestedNode<'a, G: Html>(cx: Scope<'a>, n: Node, items: &'a Signal<Vec<Node>>)
         if e.type_().contains("dragstart") {
             data_transf.set_effect_allowed("move");
             data_transf
-                .set_data("text/html", &c_item.get().id.to_string())
+                .set_data("text/html", &node_signal.get().id.to_string())
                 .unwrap();
 
-            log!(format!("Transfer {:?}", &c_item.get()));
+            log!(format!("Transfer {:?}", &node_signal.get()));
         }
-        dom.set_attribute("style", "opacity: 0.2");
+        //dom.set_attribute("style", "opacity: 0.2");
+        dom.add_class("bg-primary bg-opacity-50");
     };
 
-    let handle_dragend = |e: Event| {
-        let dom = node_ref.get::<DomNode>();
-        dom.set_attribute("style", "opacity: 1");
-        log!(format!("{:?}", e.type_()));
-    };
     let handle_dragenter = |e: Event| {
         let dom = node_ref.get::<DomNode>();
-        dom.add_class("bg-primary");
+        // dom.remove_class("");
+        dom.add_class("bg-primary bg-opacity-25");
         log!(format!("{:?}", e.type_()));
     };
 
     let handle_dragover = |e: Event| {
         let dom = node_ref.get::<DomNode>();
         e.prevent_default();
-        dom.add_class("bg-info");
+        dom.remove_class("bg-opacity-25");
+        dom.add_class("bg-primary bg-opacity-10");
     };
 
     let handle_dragleave = |e: Event| {
         let dom = node_ref.get::<DomNode>();
-        dom.remove_class("bg-info");
+        dom.remove_class("bg-primary");
         log!(format!("{:?}", e));
     };
 
+    let handle_dragend = |e: Event| {
+        let dom = node_ref.get::<DomNode>();
+        //dom.set_attribute("style", "opacity: 1");
+        dom.remove_class("bg-opacity-50");
+        dom.add_class("bg-success bg-opacity-10");
+
+        log!(format!("{:?}", e.type_()));
+    };
     let handle_drop = move |e: Event| {
+        let dom = node_ref.get::<DomNode>();
+
         let drag_event_ref: &web_sys::DragEvent = e.unchecked_ref();
         let drag_event = drag_event_ref.clone();
         let data_transf: DataTransfer = drag_event.data_transfer().unwrap();
         let data = data_transf.get_data("text/html").unwrap();
 
         log!(format!("{:?}", data.clone()));
-        log!(format!("{:?}", &c_item.get()));
+        log!(format!("{:?}", &node_signal.get()));
+        dom.remove_class("bg-primary");
+        dom.add_class("bg-warning bg-opacity-10");
 
-        let mut items = items.modify();
+        let mut items = nodes_sig.modify();
         let dragged_index = items
             .iter()
             .position(|i| i.id == data.parse::<i32>().unwrap())
             .unwrap();
-        let target_index = items.iter().position(|i| i.id == c_item.get().id).unwrap();
+        let target_index = items
+            .iter()
+            .position(|i| i.id == node_signal.get().id)
+            .unwrap();
         items.swap(dragged_index, target_index);
     };
 
-    view! {cx,  i(on:click=toggle, class=class()) (node.name)
-     (if *toggle_state.get() {
-
-         view! { cx,
-            ul(class="list-group") {
-                Keyed(
-                    iterable=children_signal,
-                    view= move |cx, x| view! { cx,
-                       li(class="list-group-item", ref=node_ref, draggable=true, on:dragstart=handle_dragstart, on:dragend=handle_dragend, on:dragenter=handle_dragenter, on:dragover=handle_dragover, on:dragleave=handle_dragleave, on:drop=handle_drop) {NestedNode(n=x, items=items)}
-                    },
-                    key=|x| x.id,
-                )
-            }
-        }
-
-    } else {
-        view! { cx, } // Now you don't
-    }
-     )
-        }
-}
-
-#[component(inline_props)]
-fn TreeNode<'a, G: Html>(cx: Scope<'a>, n: Node, items: &'a Signal<Vec<Node>>) -> View<G> {
-    /*
-    let all_nodes = use_context::<NodeState>(cx);
-    let nodes = all_nodes.nodes.get().as_ref().clone().list;
-    let x_child = n.clone();
-    let n_haschild = x_child.has_child(&nodes);
-    */
-
-    let has_child = n.has_child(&items.get());
     view! { cx,
-            li(class="list-group-item") {
-                    (if has_child {
-                        let nx = n.clone();
-
-
-                   view!(cx, NestedNode(n=nx, items=items))
-                    } else {
-                        let nx = n.clone();
-                        view! { cx,  (nx.name)}
-                    })
-
-
-            }
-
+        div(ref=node_ref, draggable=true, class="item", on:dragstart=handle_dragstart, on:dragend=handle_dragend, on:dragenter=handle_dragenter, on:dragover=handle_dragover, on:dragleave=handle_dragleave, on:drop=handle_drop) {
+            (node_signal.get().name)
+        }
     }
 }
 
 #[component]
-fn App<G: Html>(cx: Scope) -> View<G> {
+fn ContainerWidget<G: Html>(cx: Scope) -> View<G> {
+    let contents = create_signal(
+        cx,
+        vec![
+            ContentItem {
+                id: 0,
+                name: "Test item 0".to_string(),
+            },
+            ContentItem {
+                id: 1,
+                name: "Test item 1".to_string(),
+            },
+            ContentItem {
+                id: 2,
+                name: "Test item 2".to_string(),
+            },
+            ContentItem {
+                id: 3,
+                name: "Test item 3".to_string(),
+            },
+        ],
+    );
+
     let vec_nodes = vec![
         Node::new(1, None, "Node 1"),
         Node::new(2, Some(1), "Node 2"),
@@ -215,62 +186,29 @@ fn App<G: Html>(cx: Scope) -> View<G> {
         Node::new(10, Some(9), "node 10"),
     ];
 
-    let node_list = NodeList {
-        list: vec_nodes.clone(),
-    };
-
-    let node_state = NodeState {
-        nodes: create_rc_signal(node_list.clone()),
-    };
-    let node_context = provide_context(cx, node_state);
-    let root_nodes = node_context.nodes.get().get_root_nodes();
-
-    let vnodes = create_signal(cx, vec_nodes);
-    let rnodes = create_signal(cx, root_nodes.clone());
+    let node_list = create_signal(cx, vec_nodes);
 
     view! { cx,
-            div(class="py-4"){
-                div(class="container-sm"){
-                div(class="row align-items-center"){
-                    div(class="col"){
-                        div (class="card", style="width: 18rem;") {
-                            ul (class="list-group list-group-flush") {
-                                Keyed(
-                                    iterable=rnodes,
-                                    view= move |cx, x|
-                                        {
-                                            view! { cx,
-                                                    TreeNode(n=x, items=vnodes)
-
-                                                    /*
-                                                    li(class="list-group-item") {
-                                                        (if n_haschild {
-                                                            let nx = n.clone();
-                                                            view!(cx, NestedNode(nx))
-                                                        } else {
-                                                            let nx = n.clone();
-                                                            view! { cx,  (nx.name)}
-                                                        })
-                                                    }
-                                                    */
-                                                }
-                                        },
-                                    key=|x| x.id,
-                                )
-
-                            }
-                        }
-                    }
-
-                }
-
+        div(class = "container") {
+            div(class="box") {
+                Keyed(
+                    iterable=node_list,
+                    view= move |cx, item| view! { cx, Item(n = item, nodes_sig = node_list) },
+                    key=|item| item.id,
+                )
             }
-            }
+        }
     }
 }
+// modified draggable
 
 fn main() {
     console_error_panic_hook::set_once();
     console_log::init_with_level(log::Level::Debug).unwrap();
-    sycamore::render(App);
+    sycamore::render(|cx| {
+        view! { cx,
+            p { "Hello, World!" }
+                ContainerWidget()
+        }
+    });
 }
